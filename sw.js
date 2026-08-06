4813fae0baee84992b5a4eebfeb4d093cb3b5b21
mascochicle <mascochicle@users.noreply.github.com>
2026-08-06 14:12:13 -0600

    Periodos: fuera "Este mes" y "El mes pasado", entra "Ultimos 90 dias"
    Marco lo pidio asi. El 3-ago se habian quitado los 90 dias justamente
    porque los dos de mes los cubrian; ahora le da la vuelta, y tiene sentido:
    una ventana movil no depende de en que dia del mes vaya.
    
    No se pierde la vista por mes: el calendario tiene "Ver el mes", que pone
    per.t='mes', y de ahi las flechas se mueven mes por mes igual que antes.
    
    La lista queda: Hoy | 5 dias | 30 dias | 90 dias | Este año | Escoger año.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V = 'masmoney-20260806-1405';
+const V = 'masmoney-20260806-1412';
 const ARCHIVOS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
